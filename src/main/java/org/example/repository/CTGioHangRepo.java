ac4541ed3754c9adfc86221fcee83e7ce9f31679
NguyenTrungHieunt11 <hieuntph34687@fpt.edu.vn>
2026-01-13 17:01:33 +0700

    Thanh toán

--- src/main/java/org/example/repository/CTGioHangRepo.java
+++ src/main/java/org/example/repository/CTGioHangRepo.java
@@ -1,8 +1,11 @@
 package org.example.repository;
 
+import jakarta.transaction.Transactional;
 import org.example.entity.CTGioHang;
 import org.example.entity.GioHang;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +19,9 @@ public interface CTGioHangRepo extends JpaRepository<CTGioHang, String> {
             String idGH,
             String idCTSP
     );
+
+    @Modifying
+    @Transactional
+    @Query("delete from CTGioHang c where c.sanPhamChiTiet.id = ?1 and c.gioHang.khachHang.id = ?2")
+    void deleteBySpCtAndKh(String idCTGH, String id);
 }
